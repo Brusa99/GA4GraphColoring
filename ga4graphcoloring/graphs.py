4455aec516b3803a8c2ef84ca205891992361b73
Brusa99 <simone.brusatin@gmail.com>
2024-02-17 16:36:40 +0100

    graph class docs

--- ga4graphcoloring/graphs.py
+++ ga4graphcoloring/graphs.py
@@ -1,6 +1,5 @@
 import numpy as np
 import networkx as nx
-import matplotlib.pyplot as plt
 
 import warnings
 
@@ -9,7 +8,19 @@ warnings.filterwarnings("ignore", message="No data for colormapping provided")
 
 
 class Graph:
-    """(Undirected) Graph class"""
+    """(Undirected) Graph.
+
+    This class represents an undirected graph using an adjacency matrix.
+    The graph is generated randomly with a given number of vertices and a density factor, which determines the
+    probability of an edge existing between two vertices.
+
+    Attributes:
+        n_vertices (int): The number of vertices in the graph.
+        adj_matrix (np.ndarray): The adjacency matrix of the graph.
+
+    Methods:
+        display: Display the graph using networkx.
+    """
 
     def __init__(self, n_vertices: int, density_factor: float = 0.5):
         """Constructor method.
@@ -18,7 +29,7 @@ class Graph:
          vertices is given by the density_factor.
         """
         self.n_vertices = n_vertices
-        self.density_factor = density_factor
+        self._density_factor = density_factor
 
         # Randomly generate adjacency matrix, edge exists with probability `density_factor`
         adj_matrix = np.random.rand(n_vertices, n_vertices)
@@ -28,7 +39,7 @@ class Graph:
         self.adj_matrix = adj_matrix
 
     def __repr__(self):
-        return f"Graph (n_vertices={self.n_vertices}, density_factor={self.density_factor})"
+        return f"Graph (n_vertices={self.n_vertices}, density_factor={self._density_factor})"
 
     def display(self, colors: list[int] | None = None):
         """Display graph using networkx"""
